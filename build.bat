6cdbca77c3958f75918accd8adccf932410f68cd
SkCodeRains <68813335+SkCodeRains@users.noreply.github.com>
2024-05-08 19:32:06 +0530

    update the issues for the images

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
- 
 
+REM Remove the "docs" folder
 rmdir /s /q "docs"
+
 REM Define source and destination directories
 set "source_dir=dist\portfolio\browser"
 set "destination_dir=docs"
@@ -9,8 +10,8 @@ set "destination_dir=docs"
 REM Create destination directory if it doesn't exist
 if not exist "%destination_dir%" mkdir "%destination_dir%"
 
-REM Move files from source to destination
-move /Y "%source_dir%\*" "%destination_dir%"
+REM Copy files and directories from source to destination recursively
+xcopy /s /e /y "%source_dir%\*" "%destination_dir%"
 
-REM Delete the dist folder
+REM Remove the "dist" folder
 rmdir /s /q "dist"
