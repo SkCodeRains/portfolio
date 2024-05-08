79e877d46c0d6a69dc0e1a8d0628d078d06b1dae
SkCodeRains <68813335+SkCodeRains@users.noreply.github.com>
2024-05-08 18:39:20 +0530

    added build config
    Created build file and configure the building for the github projects

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+ 
+
+REM Define source and destination directories
+set "source_dir=dist\portfolio\browser"
+set "destination_dir=docs"
+
+REM Create destination directory if it doesn't exist
+if not exist "%destination_dir%" mkdir "%destination_dir%"
+
+REM Move files from source to destination
+move /Y "%source_dir%\*" "%destination_dir%"
+
+REM Delete the dist folder
+rmdir /s /q "dist"
